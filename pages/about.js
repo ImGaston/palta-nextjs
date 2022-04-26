4e613db72fbf1c54c67abb2052bad1cb1a7310cb
Gaston <corbalan.gaston@outlook.com>
2022-04-26 07:22:28 -0300

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar';
+
+const About = () => {
+    return (
+        <div>
+            <Navbar />
+            <p>Nuestras paltas son las mejores que hay en el país</p>
+        </div>
+    )
+}
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hola!</h1>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Home 
