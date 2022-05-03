f81cf6782165b49db885e7d30ae11571e2b8822a
Gaston <corbalan.gaston@outlook.com>
2022-05-03 07:59:14 -0300

    API ready to use

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar';
 
 const Home = () => {
+    const [productList, setproductList] = useState([]);
+
+    useEffect(() => {
+        //pido la info
+        window.fetch('/api/palta')
+        //lo transformo en un JSON
+        .then((response) => response.json())
+        //lo cargo en el estado de productos
+        .then(({ data, length }) => {
+            setproductList(data)
+        })
+    }, [])
+
+
     return (
         <div>
             <h1>Hola!</h1>
             <Navbar />
+            {productList.map((product) => (
+                <div>
+                    <div>{product.name}</div>
+                    <div>{product.image}</div>
+                </div>
+            ))}
         </div>
     )
 }
